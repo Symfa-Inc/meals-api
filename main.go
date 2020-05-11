8eced84dedb466d654b3dccc0cc11cad87761053
Dmitry Novikov <d.novikov@wellyes.ru>
2020-05-11 09:29:17 +0000

    Feat/meal 123

--- main.go
+++ main.go
@@ -5,11 +5,9 @@ import (
 	_ "go_api/src/config"
 	"go_api/src/mux"
 )
+
 // @title AIS Catering
 // @version 1.0.0
-// @securityDefinitions.apikey ApiKeyAuth
-// @in header
-// @name Authorization
 func main() {
 	r := mux.SetupRouter()
 	r.Run()
--- src/mux/auth/is-authenticated.go
+++ src/mux/auth/is-authenticated.go
@@ -1,7 +1,6 @@
 package auth
 
 import (
-	jwt "github.com/appleboy/gin-jwt"
 	"github.com/gin-gonic/gin"
 	users "go_api/src/models/user"
 	"go_api/src/mux/middleware"
@@ -10,17 +9,17 @@ import (
 )
 
 // IsAuthenticated godoc
-// @Summary returns user info if user is authorized
+// @Summary Returns user info if authorized
 // @Produce json
 // @Accept json
 // @Tags auth
 // @Success 200 {object} auth.IsAuthenticated
-// @Failure 404 {object} types.Error
+// @Failure 401 {object} types.Error
 // @Security ApiKeyAuth
 // @Router /is-authenticated [get]
 func IsAuthenticated(c *gin.Context) {
-	claims := jwt.ExtractClaims(c)
-	id := claims[middleware.IdentityKey]
+	claims := middleware.ExtractClaims(c)
+	id := claims[middleware.IdentityKeyID]
 	user, _ := users.GetUserByKey("id", id.(string))
 	c.JSON(http.StatusOK, auth.IsAuthenticated{
 		ID:        user.ID,
