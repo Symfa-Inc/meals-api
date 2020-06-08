9ff767e30335c6d3349967b6b2ca15ea469c2308
Dmitry Novikov <d.novikov@wellyes.ru>
2020-06-08 11:59:02 +0000

    Fix/env path

--- src/config/env.go
+++ src/config/env.go
@@ -2,36 +2,32 @@ package config
 
 import (
 	"github.com/joho/godotenv"
-	"log"
 	"os"
 )
 
 type env struct {
-	DbHost string
-	DbPort string
-	DbUser string
+	DbHost     string
+	DbPort     string
+	DbUser     string
 	DbPassword string
-	DbName string
-	ClientURL string
-	Port string
+	DbName     string
+	ClientURL  string
+	Port       string
 }
 
 // Env is env project struct
 var Env env
 
 func init() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal(err)
-	}
+	_ = godotenv.Load(".env")
 
- 	Env = env{
-		DbHost: os.Getenv("DB_HOST"),
-		DbPort: os.Getenv("DB_PORT"),
-		DbUser: os.Getenv("DB_USER"),
+	Env = env{
+		DbHost:     os.Getenv("DB_HOST"),
+		DbPort:     os.Getenv("DB_PORT"),
+		DbUser:     os.Getenv("DB_USER"),
 		DbPassword: os.Getenv("DB_PASSWORD"),
-		DbName: os.Getenv("DB_NAME"),
-		ClientURL: os.Getenv("CLIENT_URL"),
-		Port: os.Getenv("PORT"),
+		DbName:     os.Getenv("DB_NAME"),
+		ClientURL:  os.Getenv("CLIENT_URL"),
+		Port:       os.Getenv("PORT"),
 	}
 }
