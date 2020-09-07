0f97c980055e74dd15e8f8cd89c668e810bcd703
eduard <E.Gaifutdinov@wellyes.ru>
2020-09-07 15:37:08 +0300

    Fix db error

--- api/swagger/get_user.go
+++ api/swagger/get_user.go
@@ -11,10 +11,10 @@ type UserResponse struct {
 	LastName    string    `gorm:"type:varchar(20)" json:"lastName"`
 	Email       string    `gorm:"type:varchar(30);unique;not null" json:"email"`
 	Password    string    `gorm:"type:varchar(100);not null" json:"-"`
-	CompanyType string    `sql:"company_types" gorm:"type:varchar(20);null" json:"companyType"`
+	CompanyType string    `sql:"type:company_types" gorm:"type:varchar(20);null" json:"companyType"`
 	ClientID    string    `json:"clientId"`
 	CateringID  string    `json:"cateringId"`
-	Role        string    `sql:"user_roles" json:"role"`
+	Role        string    `sql:"type:user_roles" json:"role"`
 	Floor       int       `json:"floor"`
-	Status      string    `sql:"status_types" gorm:"type:varchar(10);null" json:"status"`
+	Status      string    `sql:"type:status_types" gorm:"type:varchar(10);null" json:"status"`
 }
--- domain/user.go
+++ domain/user.go
@@ -12,9 +12,9 @@ type User struct {
 	LastName    string  `gorm:"type:varchar(20)" json:"lastName"`
 	Email       string  `gorm:"type:varchar(30);not null" json:"email"`
 	Password    string  `gorm:"type:varchar(100);not null" json:"-"`
-	Role        string  `sql:"user_roles" json:"role"`
-	CompanyType *string `sql:"company_types" gorm:"type:varchar(20);null" json:"companyType"`
-	Status      *string `sql:"status_types" gorm:"type:varchar(10);null" json:"status"`
+	Role        string  `sql:"type:user_roles" json:"role"`
+	CompanyType *string `sql:"type:company_types" gorm:"type:varchar(20);null" json:"companyType"`
+	Status      *string `sql:"type:status_types" gorm:"type:varchar(10);null" json:"status"`
 }
 
 // UserClientCatering struct for joined catering and clients table
