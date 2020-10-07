4e1c4619d0dcf1c5f10bcf7be1873751f62abed9
Eduard <e.gaifutdinov@wellyes.ru>
2020-10-07 17:30:11 +0300

    Add spacing

--- backup/dump.go
+++ backup/dump.go
@@ -2,7 +2,6 @@ package backup
 
 import (
 	"bytes"
-	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -13,14 +12,18 @@ import (
 )
 
 func CreateBackup() {
-	fmt.Println("qewqs")
-	cmd := exec.Command("pg_dump", "-u", config.Env.DbUser, "-w", config.Env.DbName)
-	cmd.Stdin = strings.NewReader(config.Env.DbPassword)
 	var out bytes.Buffer
+
+	cmd := exec.Command("pg_dump", "-u", config.Env.DbUser, "-w", config.Env.DbName)
+
+	cmd.Stdin = strings.NewReader(config.Env.DbPassword)
 	cmd.Stdout = &out
+
 	err := cmd.Run()
+
 	if err == nil {
-		file, err := os.Create("dumps/dump-" + time.Now().UTC().String() + ".dump")
+		file, err := os.Create("dumps/dump-" + time.Now().UTC().String() + ".psql")
+
 		if err != nil {
 			log.Println(err)
 		}
